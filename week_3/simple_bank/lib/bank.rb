28e5320eb0358fc3362dfebff6fb584ba31ee5f3
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-27 14:17:52 +0000

    expanded functionality of the BankAccount class and increased complexity of tests

--- week_3/simple_bank/lib/bank.rb
+++ week_3/simple_bank/lib/bank.rb
@@ -1,5 +1,17 @@
 class BankAccount
+  def initialize(starting_funds=0)
+    @balance = starting_funds
+  end
   def deposit(amount)
     "You deposited £#{amount} into your account"
   end
+
+  def withdraw(amount)
+    "You withdrew £#{amount}"
+  end
+
+  def view_bal
+    "Your current balance is £#{@balance}"
+  end
+
 end
--- week_3/simple_bank/spec/bank_spec.rb
+++ week_3/simple_bank/spec/bank_spec.rb
@@ -10,4 +10,22 @@ describe BankAccount do
       expect(my_account.deposit(42)).to eq "You deposited £42 into your account"
     end
   end
+
+  describe '#withdraw' do
+    it 'can notify the user on how much they have withdrawn' do
+      my_account = BankAccount.new
+      expect(my_account.withdraw(20)).to eq "You withdrew £20"
+    end
+
+    it 'the account balance should be correct after withdrawal' do
+      my_account = BankAccount.new
+      my_account.deposit(30)
+      my_account.withdraw(25)
+      expect(my_account.view_bal).to eq "Your current balance is £5"
+    end
+
+    it 'warns the customer that they hace insufficient funds to make the withdrawal' do
+    end
+
+  end
 end
