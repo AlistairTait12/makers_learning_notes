cbe81268bec9e0805ae012d64c12ec09ff5f66e3
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-27 14:00:33 +0000

    added another test to further validate functionality of the instance method 'deposit'

--- week_3/simple_bank/spec/bank_spec.rb
+++ week_3/simple_bank/spec/bank_spec.rb
@@ -5,5 +5,9 @@ describe BankAccount do
       my_account = BankAccount.new
       expect(my_account.deposit(100)).to eq "You deposited £100 into your account"
     end
+    it 'can notify the user that any amount of funds have been deposited' do
+      my_account = BankAccount.new
+      expect(my_account.deposit(42)).to eq "You deposited £42 into your account"
+    end
   end
 end
