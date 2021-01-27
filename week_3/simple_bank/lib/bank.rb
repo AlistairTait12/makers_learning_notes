c99199277b9fd65bd9f5e969c3cbe9395d5ddcab
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-27 13:58:01 +0000

    passed first test, to notify customer that they deposited an example amount into their account

---
+++ week_3/simple_bank/lib/bank.rb
@@ -0,0 +1,5 @@
+class BankAccount
+  def deposit(amount)
+    "You deposited £#{amount} into your account"
+  end
+end
--- week_3/simple_bank/spec/bank_spec.rb
+++ week_3/simple_bank/spec/bank_spec.rb
@@ -3,7 +3,7 @@ describe BankAccount do
   describe '#deposit' do
     it 'can notify the user that the funds have been deposited' do
       my_account = BankAccount.new
-      expect(my_account.deposit(100)).to eq "You deposited £10 into your account"
+      expect(my_account.deposit(100)).to eq "You deposited £100 into your account"
     end
   end
 end
