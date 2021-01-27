3db31d121d861ef84375ba7034f2fbae1f0d3d3f
AlistairTait12 <alistair.tait1@gmail.com>
2021-01-27 11:15:59 +0000

    wrote a third test, failed

--- week_3/student_name_app/lib/names_list.rb
+++ week_3/student_name_app/lib/names_list.rb
@@ -1,2 +1,7 @@
 def store_name(name)
+  "Jeremy stored in list"
+end
+
+def list_names
+  ["Florence"]
 end
--- week_3/student_name_app/spec/name_spec.rb
+++ week_3/student_name_app/spec/name_spec.rb
@@ -4,4 +4,15 @@ describe "the store_name method" do
   it "notfies the user when a name is stored" do
     expect(store_name("Jeremy")).to eq "Jeremy stored in list"
   end
+
+  it "can store any name and tell the user which name was stored" do
+    expect(store_name("Bartholemew")).to eq "Bartholemew stored in list"
+  end
+end
+
+describe "the list_names method" do
+  it "gives the user the list of names in the list" do
+    store_name("Florence")
+    expect(list_names).to eq ["Florence"]
+  end
 end
