c56d9c30fb3ab849ab130da4c664d5f28e5da7e3
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-01 17:00:44 +0000

    refactored User into two classes with my current understanding of forwarding

--- week_4/user/user.rb
+++ week_4/user/user.rb
@@ -6,17 +6,24 @@ class User
     @password = password
   end
 
-  def get_info
-    
+  def profile
+    "#{@name}, born in #{birth_year}: #{@bio}"
   end
+  
+  private
+
+  def birth_year
+    Time.new.year - @age
+  end
+
 end
 
 class UserAccount
   def initialize
-    @account = User.new
+    @account = User.new('Jeremy Johnson', 'Coding enthusiast', 34, 'password1')
   end
 
   def authenticate(candidate_password)
-    
+    return true if candidate_password == @account.password
   end
 end
