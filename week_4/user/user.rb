a63b85d5566b84edfc23589d88eb1237b3d87326
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-01 14:33:42 +0000

    started on user account exercise

--- week_4/user/user.rb
+++ week_4/user/user.rb
@@ -1,7 +1,22 @@
 class User
-  
+  def initialize(name, bio, age, password)
+    @name = name
+    @bio = bio
+    @age = age
+    @password = password
+  end
+
+  def get_info
+    
+  end
 end
 
-class LockedUser
+class UserAccount
+  def initialize
+    @account = User.new
+  end
 
+  def authenticate(candidate_password)
+    
+  end
 end
